fde1c5b2c061b4b622e51a31ed5a5c866386c625
DatalakeLab <88815311+DatalakeLab@users.noreply.github.com>
2021-08-18 19:57:49 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Build import cythonize
+from Cython.Distutils import build_ext 
+import numpy as np
+
+extensions = [
+    Extension("c_xgb_test", 
+        sources = ["c_xgb_test.pyx", "c_xgb/c_xgb.cpp"], 
+        language = "c++",
+        extra_compile_args = ["-O3", "-std=c++11", "-Wno-unused-function"]), 
+]
+
+setup(
+    cmdclass = {'build_ext': build_ext},
+    ext_modules = cythonize(
+        extensions
+    ),
+) 
