b82b7146f8725aea751095e661586ebe0642ac7b
DatalakeLab <88815311+DatalakeLab@users.noreply.github.com>
2021-08-18 19:56:28 -0300

    Create c_xgb_test.py

---
+++ c_xgb_test.py
@@ -0,0 +1,21 @@
+import c_xgb_test
+
+print ("Linear Regression")
+print ("**********************************************")
+c_xgb_test.test_xgb_regression(n_samples = 10000, n_features = 20, n_estimators = 10, depth = 5)
+print ("**********************************************")
+c_xgb_test.test_xgb_regression(n_samples = 10000, n_features = 20, n_estimators = 10, depth = 10)
+print ("**********************************************")
+c_xgb_test.test_xgb_regression(n_samples = 10000, n_features = 20, n_estimators = 100, depth = 5)
+print ("**********************************************")
+c_xgb_test.test_xgb_regression(n_samples = 10000, n_features = 20, n_estimators = 100, depth = 10)
+
+print ("Logistic Regression")
+print ("**********************************************")
+c_xgb_test.test_xgb_logistic_regression(n_samples = 10000, n_features = 20, n_estimators = 10, depth = 5)
+print ("**********************************************")
+c_xgb_test.test_xgb_logistic_regression(n_samples = 10000, n_features = 20, n_estimators = 10, depth = 10)
+print ("**********************************************")
+c_xgb_test.test_xgb_logistic_regression(n_samples = 10000, n_features = 20, n_estimators = 100, depth = 5)
+print ("**********************************************")
+c_xgb_test.test_xgb_logistic_regression(n_samples = 10000, n_features = 20, n_estimators = 100, depth = 10)
