6fd007aa52d044e6f37449ef3bcd32bdb6e43343
DatalakeLab <88815311+DatalakeLab@users.noreply.github.com>
2021-08-19 01:36:10 -0300

    Create c_xgb_test.pyx

--- c_xgb_test.pyx
+++ c_xgb_test.pyx
@@ -13,7 +13,28 @@ import time
 from sklearn.datasets import make_regression, make_classification
 from libc.stdio cimport printf
 import psutil
+import resource
+import memory_profiler
 
+
+def uso(mensagem):
+    usage = resource.getrusage(resource.RUSAGE_SELF)
+    print ("**********************************************")
+    print (mensagem)
+    mem_usage = memory_profiler.memory_usage()[0]
+    print ("**** Memory Usage" + str(mem_usage))
+    for name, desc in [
+     ('ru_utime', 'User time'),
+     ('ru_stime', 'System time'),
+     ('ru_maxrss', 'Max. Resident Set Size'),
+     ('ru_ixrss', 'Shared Memory Size'),
+     ('ru_idrss', 'Unshared Memory Size'),
+     ('ru_isrss', 'Stack Size'),
+     ('ru_inblock', 'Block inputs'),
+     ('ru_oublock', 'Block outputs'),
+     ]:
+     print '%-25s (%-10s) = %s' % (desc, name, getattr(usage, name))
+    
 def test_xgb_regression(n_samples = 10000, n_features = 20, n_estimators = 3, depth = 11):
     '''
     correctness and performance test for C-xgb for regression tasks
@@ -21,15 +42,19 @@ def test_xgb_regression(n_samples = 10000, n_features = 20, n_estimators = 3, de
     cdef float base_score = 0.5
 
     #creating dataset
+    uso("Creating DataSet: Begin")
     x, y = make_regression(n_samples = n_samples, n_features = n_features, n_informative = n_features - 1, random_state = 1)
-    
+    uso("Creating DataSet: End")
+
     #training xgb classifier on random dataset
     def create_xgb():
             
+        uso("Creating XGBRegressor: Begin")
         model = xgb.XGBRegressor(max_depth = depth, learning_rate = 0.1, n_estimators = n_estimators, silent = True, 
                                  objective = 'reg:linear', n_jobs = 8, min_child_weight = 1, 
                                   subsample = 0.8, colsample_bytree = 0.8, random_state = 5, missing = np.nan, base_score = base_score)
-        
+        uso("Creating XGBRegressor: End")
+
         model.fit(x, y)
 
         return model
@@ -53,8 +78,6 @@ def test_xgb_regression(n_samples = 10000, n_features = 20, n_estimators = 3, de
     cdef int j, q, N = 10
 
     #performing tests
-    total_p=0
-    total_c=0
     for i in xrange(n_samples):
         for j in xrange(n_features): 
             x_cython[j] = x[i][j]#np.around(x[i][j], 3)
@@ -68,33 +91,22 @@ def test_xgb_regression(n_samples = 10000, n_features = 20, n_estimators = 3, de
         assert(abs(preds_xgb - preds_c_xgb) < 1e-3)
         
         #time measurement for CythonXGB
-        memory_c_xgp = psutil.Process().memory_info().rss / (1024 * 1024)
         start = time.time()
         for q in xrange(N):
             model_c.predict(x_cython, n_estimators)
-            memory_c_xgp += (psutil.Process().memory_info().rss / (1024 * 1024))
         time_c_xgb += (time.time() - start)
-        memory_c_xgp = memory_c_xgp/N
-        total_c += memory_c_xgp
 
         #time measurement for XGBoost
-        memory_p_xgp = psutil.Process().memory_info().rss / (1024 * 1024)
         start = time.time()
         for q in xrange(N):
             model.predict(reshaped_sample)
-            memory_p_xgp += (psutil.Process().memory_info().rss / (1024 * 1024))
         time_xgb += (time.time() - start)
-        memory_p_xgp = memory_p_xgp/N
-        total_p += memory_p_xgp
 
-    total_c = total_c/n_samples
-    total_p = total_p/n_samples
+    
     print 'n_samples = %d | n_estimators = %d | max_depth = %d | objective = %s' % (n_samples, n_estimators, depth, 'reg:linear')
     print "XGBoost mean time in ms: %f" % (time_xgb*1000)
-    print "Python XGBoost Memory: %f" % (total_p)
 
     print "C_XGBoost mean time in ms: %f" % (time_c_xgb*1000)
-    print "Cython XGBoost Memory: %f" % (total_c)
 
     print "ACCELERATION IS %f TIMES\n" % (time_xgb / time_c_xgb)
 
